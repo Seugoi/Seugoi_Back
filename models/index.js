b317d07ff9662f3626fbf0b8cc4c85f211042392
rlagkdms <ithaeun@naver.com>
2024-07-07 23:54:59 +0900

    Feat : 채팅방(chatRoom)  db model 구현

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const User = require("./users");
 const Study = require("./study");
 const JoinStudy = require("./joinStudy");
+const ChatRoom = require("./chatRoom");
 
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config.js")[env];
@@ -28,9 +29,11 @@ db.sequelize = sequelize;
 db.User = User;
 db.Study = Study;
 db.JoinStudy = JoinStudy;
+db.ChatRoom = ChatRoom;
 
 User.init(sequelize);
 Study.init(sequelize);
 JoinStudy.init(sequelize);
+ChatRoom.init(sequelize);
 
 module.exports = db;
