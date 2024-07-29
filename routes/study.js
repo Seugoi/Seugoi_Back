f48883a5c63e059345f0d81fd10c092c59677f72
bhark065 <w2216@e-mirim.hs.kr>
2024-07-29 17:42:29 +0900

    Refactor: 이미지 업로드 코드 수정

--- routes/study.js
+++ routes/study.js
@@ -5,20 +5,16 @@ const likeMiddleware = require('../controllers/likeStudy');
 const joinMiddleware= require('../controllers/joinStudy');
 const multer = require('multer');
 
-const storage = multer.memoryStorage({ 
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
     fileFilter: (req, file, cb) => {
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
             return cb(new Error('이미지 파일만 업로드 가능!'), false);
         }
         cb(null, true);
-    },
-    // 파일 이름 설정: 원본 파일 이름 사용
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
     }
 });
-
-const upload = multer({ storage: storage });
 
 router.post('', upload.single('image'), studyMiddleware.createStudy); // 스터디 생성
 router.get('', studyMiddleware.allStudy); // 모든 스터디 조회
