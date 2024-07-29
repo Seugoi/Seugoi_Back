ea3875ca3ab666cd9a0e36558b6c8ae239eea599
ChoiBoRam <111477278+bhark065@users.noreply.github.com>
2024-07-29 17:55:22 +0900

    Style: 사진 업로드 갯수 수정

--- routes/task.js
+++ routes/task.js
@@ -13,7 +13,7 @@ const upload = multer({
     }
 });
 
-router.post('', upload.array('images', 3), taskMiddleware.createComment); // 과제 댓글 등록
+router.post('', upload.array('images', 5), taskMiddleware.createComment); // 과제 댓글 등록
 router.get('', taskMiddleware.allTaskComment); // 댓글 전체 조회
 router.get('/:study_id', taskMiddleware.idComment); // 특정 스터디 댓글 조회
 
