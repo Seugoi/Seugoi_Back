c54e074f8eddcf2b012b4dd6515d89d7fcd9a321
bhark065 <w2216@e-mirim.hs.kr>
2024-08-09 00:38:46 +0900

    Refactor: attributes 제거

--- utils/getUserMap.js
+++ utils/getUserMap.js
@@ -3,10 +3,7 @@ const { User } = require('../models');
 // 유저
 async function getUserMap(userIds) {
     const users = await User.findAll({
-        attributes: ['id', 'nickname', 'profile_img_url'],
-        where: {
-            id: userIds
-        }
+        where: { id: userIds }
     });
 
     return users.reduce((acc, user) => {
